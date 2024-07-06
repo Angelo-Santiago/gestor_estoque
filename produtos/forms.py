24859d8842ed25668ce4a27ff22ea5781f3ea3e2
vfsphotos <viniciusfphotographer@gmail.com>
2024-07-05 21:05:45 -0300

    Categorias e Produtos

--- produtos/forms.py
+++ produtos/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
-from .models import Embalagem, Local
+from .models import Categoria, Embalagem, Local, Produto
 
 
 class LocalForm(forms.ModelForm):
@@ -13,3 +13,15 @@ class EmbalagemForm(forms.ModelForm):
     class Meta:
         model = Embalagem
         fields = ['name', 'sigla']
+
+
+class CategoriaForm(forms.ModelForm):
+    class Meta:
+        model = Categoria
+        fields = ['name']
+
+
+class ProdutoForm(forms.ModelForm):
+    class Meta:
+        model = Produto
+        fields = ['nome', 'categoria', 'embalagens', 'estoque_minimo', 'estoque_maximo']
--- produtos/urls.py
+++ produtos/urls.py
@@ -1,15 +1,23 @@
 from django.urls import path
 
 from .views import (
+    adicionar_categoria,
     adicionar_embalagem,
     adicionar_local,
+    adicionar_produtos,
+    editar_categoria,
     editar_embalagem,
     editar_local,
+    editar_produto,
+    excluir_categoria,
     excluir_embalagem,
     excluir_local,
+    excluir_produto,
     home,
+    listar_categorias,
     listar_embalagens,
     listar_locais,
+    listar_produtos,
 )
 
 urlpatterns = [
@@ -18,8 +26,16 @@ urlpatterns = [
     path('locais/adicionar/', adicionar_local, name='adicionar_local'),
     path('embalagens/', listar_embalagens, name='listar_embalagens'),
     path('embalagens/adicionar/', adicionar_embalagem, name='adicionar_embalagem'),
+    path('produtos/', listar_produtos, name='listar_produtos'),
+    path('produtos/adicionar/', adicionar_produtos, name='adicionar_produtos'),
+    path('categorias/', listar_categorias, name='listar_categorias'),
+    path('categorias/adicionar/', adicionar_categoria, name='adicionar_categoria'),
     path('locais/<int:id>/editar', editar_local, name='editar_local'),
     path('locais/<int:id>/excluir', excluir_local, name='excluir_local'),
     path('embalagem/<int:id>/editar', editar_embalagem, name='editar_embalagem'),
     path('embalagem/<int:id>/excluir', excluir_embalagem, name='excluir_embalagem'),
+    path('categoria/<int:id>/editar', editar_categoria, name='editar_categoria'),
+    path('categoria/<int:id>/excluir', excluir_categoria, name='excluir_categoria'),
+    path('produtos/<int:id>/editar', editar_produto, name='editar_produto'),
+    path('produtos/<int:id>/excluir', excluir_produto, name='excluir_produto')
 ]
